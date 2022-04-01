e1c6216b3cf44396e962a28ed1394d424170ab5a
jaykopp <jacobnasjodin@gmail.com>
2022-04-01 23:13:03 +0200

    Added starter scripts

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -651,10 +651,10 @@
 #define FLAG_HIDDEN_ITEM_ROUTE_123_RARE_CANDY                (FLAG_HIDDEN_ITEMS_START + 0x6E)
 #define FLAG_HIDDEN_ITEM_ROUTE_105_BIG_PEARL                 (FLAG_HIDDEN_ITEMS_START + 0x6F)
 
-#define FLAG_UNUSED_0x264  0x264 // Unused Flag
-#define FLAG_UNUSED_0x265  0x265 // Unused Flag
-#define FLAG_UNUSED_0x266  0x266 // Unused Flag
-#define FLAG_UNUSED_0x267  0x267 // Unused Flag
+#define FLAG_FERNET_TOWN_SPEECH         0x264 // New Flag
+#define FLAG_HIDE_FERNET_TOWN_TREECKO   0x265 // New Flag
+#define FLAG_HIDE_FERNET_TOWN_TORCHIC   0x266 // New Flag
+#define FLAG_HIDE_FERNET_TOWN_MUDKIP    0x267 // New Flag
 #define FLAG_UNUSED_0x268  0x268 // Unused Flag
 #define FLAG_UNUSED_0x269  0x269 // Unused Flag
 #define FLAG_UNUSED_0x26A  0x26A // Unused Flag
